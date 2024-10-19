07979c4e1c184d899b341c5dd8db4120253db624
Bill Njoroge <bnjorog2@ugradx.cs.jhu.edu>
2024-10-19 01:59:24 -0400

    adding test for all that

--- remove.c
+++ remove.c
@@ -1,3 +1,17 @@
+struct Point {
+    int x;
+    int y;
+};
 
+struct Rectangle {
+    struct Point top_left;
+    struct Point bottom_right;
+};
 
-int matrix[3][3];
+struct Point p1;
+struct Rectangle rect;
+
+struct Array {
+    int data[2][2];
+};
+struct Array p2[2][2];
